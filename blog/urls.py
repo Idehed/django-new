e53358f26af9048740531b320acfadf64a0ddd95
Idehed anton <anton.idehed@telia.com>
2024-04-07 16:38:53 +0000

    migrate

--- blog/urls.py
+++ blog/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 from my_site import views as index_views
 
 urlpatterns = [
-    path('', index_views.index, name='index'),
+    path('index/', index_views.index, name='index'),
     path('admin/', admin.site.urls),
     
 ]
